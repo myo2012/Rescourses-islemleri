45024b2143193b7cc149b606fbc2a9ee853b86d3
myo2012 <42101273+myo2012@users.noreply.github.com>
2018-08-05 01:35:11 +0300

    Add files via upload

---
+++ RescoursesIslemleri/Form1.cs
@@ -0,0 +1,64 @@
+﻿using RescoursesLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RescoursesIslemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Çıkar(string dosyaadi)
+        {
+            byte[] dosya = Properties.Resources.setup; //Rescourses daki dosyayı byte dizisine aktarır
+            System.IO.FileStream fs = new System.IO.FileStream(dosyaadi, FileMode.CreateNew);//FileStream sınıfı ile aldığı dosyaadi paramaetresine göre dosya oluşturur
+            progressBar1.Maximum = dosya.Length;//Dizinin uzunluğunu progressbar1 in maximum değerini belirtir.
+            foreach (byte b in dosya)
+            {
+                fs.WriteByte(b);//Döngü ile oluşturulan byte dizisini dosyaadi üzerisine yazar
+                progressBar1.Value++;//byte yazdırıldıkça progressbar ın valuesi arttırılır ve ekrana yansır
+            }
+            fs.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog kaydet = new SaveFileDialog();
+            kaydet.Filter = "Uygulama Dosyası (*.exe) |*.exe";
+            if (kaydet.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = kaydet.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Çıkar(textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Dosya dosya = new Dosya();
+            dosya.ProgressChanged += Dosya_ProcChanged;
+            dosya.Cikar("Setup.exe");
+
+        }
+
+        void Dosya_ProcChanged(object sender, RescoursesLib.Sonuclar e)
+        {
+            progressBar1.Maximum = e.Toplam;
+            progressBar1.Value = e.Anlık;
+        }
+    }
+}
---
+++ RescoursesLib/Dosya.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RescoursesLib
+{
+    public class Dosya
+    {
+        System.IO.FileStream fs;
+        public delegate void ProgressHandle(object sender, Sonuclar e);
+
+        public event ProgressHandle ProgressChanged;
+
+        public void Cikar(string filename)
+        {
+
+            int toplam = 0;
+            fs = new System.IO.FileStream(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + filename, System.IO.FileMode.OpenOrCreate);
+            byte[] dosya = Properties.Resources.setup;
+            Sonuclar i = new Sonuclar();
+            i.Toplam = dosya.Length;
+            foreach (byte b in dosya)
+            {
+                i.Anlık = toplam;
+                fs.WriteByte(b);
+                ProgressChanged(new object(), i);
+                toplam++;
+            }
+            fs.Close();
+        }
+    }
+    public class Sonuclar : EventArgs
+    {
+        public int Anlık { get; set; }
+        public int Toplam { get; set; }
+    }
+}
